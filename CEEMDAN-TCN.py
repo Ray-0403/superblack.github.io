5a514f5046dd75e20ef90f2c58bd283774e6ec91
superblackbad <101623827+superblackbad@users.noreply.github.com>
2022-03-15 15:51:36 +0800

    Add files via upload

---
+++ CEEMDAN-TCN.py
@@ -0,0 +1,157 @@
+import math
+import matplotlib.pyplot as plt
+from PyEMD import CEEMDAN
+import numpy as np
+import pandas as pd
+from tensorflow.keras import Input, Model
+from tensorflow.keras.layers import Dense
+from tcn import TCN
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+import pylab as mpl
+
+
+def RMSE(test, predicted):
+    rmse = math.sqrt(mean_squared_error(test, predicted))
+    return (rmse)
+
+def MAPE(Y_true, Y_pred):
+    return (np.mean(np.abs((Y_true - Y_pred) / Y_true)) * 100)
+
+def imf_data(data, look_back):
+    X1 = []
+    for i in range(look_back, len(data)):
+        X1.append(data[i - look_back:i])
+    X1.append(data[len(data) - 1:len(data)])
+    X_train = np.array(X1)
+    return X_train
+
+def create_dataset(dataset, look_back):
+    dataX, dataY = [], []
+    for i in range(0, len(dataset) - look_back, 12):
+        a = dataset[i:(i + look_back), 0]
+        dataX.append(a)
+        dataY.append(dataset[(i + look_back):(i + look_back + 12), 0])
+    return np.array(dataX), np.array(dataY)
+
+def data_split_LSTM(trainX,  testX):
+    trainX = np.reshape(trainX, (trainX.shape[0], 36, 1))  
+    testX = np.reshape(testX, (testX.shape[0], 36, 1))
+    return (trainX,  testX)
+
+if __name__ == '__main__':
+    TEC= pd.read_csv('', usecols=[1], engine='python', skipfooter=0)
+    dataset=TEC.values
+    dataset=dataset.astype('float32')
+
+    scaler=MinMaxScaler(feature_range=(0,1))
+    dataset=scaler.fit_transform(dataset)
+
+    trials = 20
+    ceemdan = CEEMDAN(trials=trials)
+    imfs = ceemdan.ceemdan(dataset.reshape(-1), None, 3)
+
+
+    fig = plt.figure(figsize=(18,12))
+    plt.subplot(11, 1, 1)
+    plt.subplots_adjust(hspace=0.5)
+    plt.tick_params(labelsize=13)
+    plt.plot(dataset,color='tab:blue')
+    plt.ylabel('Signal', fontsize=18)
+    fig.align_ylabels()
+    i = 1
+    for imf in imfs:
+        plt.tight_layout()
+        fig.align_ylabels()
+        plt.subplot(len(imfs) + 1, 1, i + 1)
+        plt.ylabel('IMF' + str(i), fontsize=18)
+        if i == 1:
+            plt.tick_params(labelsize=13)
+            plt.plot(imf,color='tab:orange')
+        if i == 2:
+            plt.tick_params(labelsize=13)
+            plt.plot(imf, color='tab:green')
+        if i == 3:
+            plt.tick_params(labelsize=13)
+            plt.plot(imf,color='tab:red')
+        if i == 4:
+            plt.tick_params(labelsize=13)
+            plt.plot(imf, color='tab:purple')
+        if i == 5:
+            plt.tick_params(labelsize=13)
+            plt.plot(imf,color='tab:brown')
+        if i == 6:
+            plt.tick_params(labelsize=13)
+            plt.plot(imf, color='tab:olive')
+        if i == 7:
+            plt.tick_params(labelsize=13)
+            plt.plot(imf, color='tab:red')
+        if i == 8:
+            plt.tick_params(labelsize=13)
+            plt.plot(imf, color='tab:orange')
+        if i == 9:
+            fig.align_ylabels()
+            mpl.rcParams['font.sans-serif'] = ['Times New Roman']
+            plt.rcParams['axes.unicode_minus'] = False
+            plt.ylabel('Res', fontsize=18)
+            mpl.rcParams['font.sans-serif'] = ['SimHei']
+            plt.rcParams['axes.unicode_minus'] = False
+            plt.xlabel('Time/2h', fontsize=18)
+            plt.plot(imf, color='tab:purple')
+        i += 1
+    plt.tick_params(labelsize=15)
+    plt.show()
+
+    tool=np.zeros([220*12,1])
+    for imf in imfs:
+        data = imf_data(imf, 1)
+        train_size = 61368
+        train, test = data[0:train_size, :], data[train_size:len(data), :]
+        testX, testY = create_dataset(test, 36)
+        testY = testY.reshape(-1, 1)
+        tool = tool + testY
+    a=[]
+    j=1
+    for imf in imfs:
+        print('-' * 45)
+        print('This is ' + str(j) + ' time(s)')
+        print('*' * 45)
+        data = imf_data(imf, 1)
+        train_size = 61368
+        train, test = data[0:train_size, :], data[train_size:len(data), :]
+        trainX, trainY = create_dataset(train, 36)
+        testX, testY = create_dataset(test, 36)
+        trainX, testX = data_split_LSTM(trainX, testX)
+        inp= Input(shape=(36,1))
+        m=TCN()(inp)
+        m=Dense(12,activation='linear')(m)
+        model=Model(inputs=[inp],outputs=[m])
+        model.summary()
+        model.compile('adam','mae')
+        print('Train...')
+        model.fit(trainX,trainY,epochs=500,verbose=2,batch_size=36)
+        prediction_Y = model.predict(testX)  # (47,12)
+        a.append(prediction_Y)
+        j += 1
+
+    a = np.array(a)
+    prediction = [0.0 for i in range(len(tool))]
+    prediction = np.array(prediction)
+    prediction=prediction.reshape(220,12)
+    for i in range(220):
+        t = 0.0
+        for imf_prediction in a:
+            t += imf_prediction[i][:]
+        prediction[i,:] = t
+    prediction = prediction.reshape(-1,1)
+
+    true = scaler.inverse_transform(tool)
+    prediction = scaler.inverse_transform(prediction)
+
+    rmse = format(RMSE(true, prediction), '.4f')
+    mape = format(MAPE(true, prediction), '.4f')
+    r2 = format(r2_score(true, prediction), '.4f')
+    mae = format(mean_absolute_error(true, prediction), '.4f')
+    print('RMSE:' + str(rmse) + '\n' + 'MAE:' + str(mae) + '\n' + 'MAPE:' + str(mape) + '\n' + 'R2:' + str(r2))
